eef60e2e4be6fb92f5deb5cf5758c2767f5fbb5f
richard-ryu <rzh1008@gmail.com>
2020-04-26 05:43:15 +0100

    67page

--- card_game/card_game.c
+++ card_game/card_game.c
@@ -5,7 +5,7 @@ int main(void) {
 	char card_name[3];
 	puts("카드 이름을 입력하세요: ");
 	scanf("%2s", card_name);
-	int val = 0;
+	int val = 0, cnt = 0;
 
 	if (card_name[0] == 'K') {
 		val = 10;
@@ -19,7 +19,14 @@ int main(void) {
 		val = atoi(card_name);
 	}
 
-	printf("카드값은 다음과 같습니다: %i\n", val);
+	if (3 <= val && val <= 6) {
+		puts("카운트를 하나 올립니다.\n");
+		cnt++;
+	} else if (val == 10 ) {
+		puts("카운트를 하나 내립니다.\n");
+		cnt--;
+	}
+
 	return 0;
 }
 	
